6a32be8e7969ea5e6c0515f755ea4b2f98e304c5
kumarigeetanjli <84951879+kumarigeetanjli@users.noreply.github.com>
2023-01-04 12:22:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-// complete the given function
+const palindromeChecker = (str) => {
+  let temp = "";
+  str = str.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+    temp += str[i];
+  }
+  for (let i = 0; i < temp.length / 2; i++) {
+    if (temp[i] != temp[temp.length - 1 - i]) return false;
+  }
+  return true;
+};
 
-function palindrome(str){
-
-}
-module.exports = palindrome
+module.exports = palindromeChecker;
